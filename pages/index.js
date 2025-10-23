f3f452e78ac23b5f98b75b93e0fc191cc46ca79b
mikaelcantini <84602540+MikaelCantini@users.noreply.github.com>
2025-10-23 15:01:17 +0000

    Alterando menssagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>!!! MEU PERFIL , TUDO SOBRE MIM !!!</h1>
+    return <h1>!!! MEU PERFIL , TUDO SOBRE MIM !!! <strong>NOVIDADES EM BREVE</strong> </h1>
 }
 
 export default Home;
